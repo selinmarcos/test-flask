5f01101b8d171b7c8b82b7a4144af449f0a53fd6
Marcos Selin <marcosselinm@gmail.com>
2022-02-08 14:22:49 -0400

    modified

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import request
 import pandas as pd
 from datetime import datetime as dt
 
-data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
+# data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
 
-series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
-for m in data.index:
-    series.loc[data.loc[m].s:data.loc[m].e] = m
+# series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
+# for m in data.index:
+#     series.loc[data.loc[m].s:data.loc[m].e] = m
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    year = int(request.args['year'])
+    fecha = int(request.args['fecha'])
     try:
-        return series.loc[year]
+        return fecha
     except KeyError:
-        return f'Invalid input ({series.index.min()} - {series.index.max()})'
+        return "INVALID"
